3fe14d04735555ca11339354184aff1a3bb388f0
CFChristianF <christianfernando671@gmail.com>
2024-10-31 23:33:03 +0700

    Third commit

--- app/src/main/java/com/example/catlist/AboutMe.kt
+++ app/src/main/java/com/example/catlist/AboutMe.kt
@@ -11,7 +11,6 @@ class AboutMe : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_me)
-        //change
         supportActionBar?.hide()
 
         val backButton = findViewById<ImageView>(R.id.back_button)
